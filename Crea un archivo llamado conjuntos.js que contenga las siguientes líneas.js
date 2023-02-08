2ddddbe162f7f5c1d4e765774d3e419e28e3ed6e
cocarls <carloscoronadouribe@gmail.com>
2023-02-08 14:45:53 -0500

    los sets

---
+++ Crea un archivo llamado conjuntos.js que contenga las siguientes líneas.js
@@ -0,0 +1,10 @@
+//Un nuevo Set con los nombres de tu familia
+var familia = new Set(["carlos", "ana", "daniel", "eva", "madeleleine"]);
+
+//Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+familia.add("carlos")
+
+//Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+familia.add("Javascript")
+console.log(familia)
+
